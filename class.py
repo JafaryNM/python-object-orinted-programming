0921079456a13b080e95519cb338d19e18fef73c
JafaryNm <jaffarymdegela@gmail.com>
2022-07-07 11:27:55 +0300

    Object and Class created

---
+++ class.py
@@ -0,0 +1,17 @@
+class Item:
+    pass
+
+
+item1= Item()
+item1.name='Beer'
+item1.price=3000
+item1.location='Arusha'
+item1.quantity=200
+
+
+########################## CHECKING DATATYPES FOR DATA #################
+
+print (type(item1.name))
+print (type(item1.price))
+
+    
