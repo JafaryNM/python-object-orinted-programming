0cb7a407ec88e33360296bf4b5ad9b953aa384be
JafaryNm <jaffarymdegela@gmail.com>
2022-07-07 13:36:10 +0300

    Class constractor is updated

---
+++ constractor.py
@@ -0,0 +1,54 @@
+class Item:
+    
+    def __init__(self, name:str,price:float,quantity=0):
+        
+    ## Constractor Methods is called when object is created
+    ## Print(f"Instance is created: {name} ,{price}, {quantity}")
+    ## Assign to each object created
+    ## Avoid to multipy number by negative number 
+    ## Validate data type 
+    
+    ########## RUN VALIDATE TO INCOMING ARGUMENTS ##########
+    
+     assert price>=0 , f"Price {price} is not greater than zero!!"
+     assert quantity>=0 , f"Quatity {quantity} is not greater than zero !!"
+    
+    
+     
+    
+    ######### ASSIGN SELF TO OBJECT ##########
+     self.name=name
+     self.price=price
+     self.quantity=quantity
+      
+      
+    ## Modified methods to receive attributies 
+    
+    def calculate_price(self):
+        
+        return self.price * self.quantity
+
+
+
+item1=Item('Laptop',300,3)
+item2=Item('Phone',90,3)
+item2.has_numpad=False
+
+print(item1.name)
+print(item2.name)
+print(item1.calculate_price())
+print(item2.calculate_price())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
